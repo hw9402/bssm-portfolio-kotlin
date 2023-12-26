21a74ffbaa674276742e33637f83471503ec4f97
마현우 <mhw35689402@gmail.com>
2023-12-26 09:51:20 +0900

    init

---
+++ src/main/kotlin/bssmwintercamp/bssmportfolio/BssmPortfolioApplication.kt
@@ -0,0 +1,11 @@
+package bssmwintercamp.bssmportfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BssmPortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<BssmPortfolioApplication>(*args)
+}
---
+++ src/test/kotlin/bssmwintercamp/bssmportfolio/BssmPortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package bssmwintercamp.bssmportfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BssmPortfolioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
