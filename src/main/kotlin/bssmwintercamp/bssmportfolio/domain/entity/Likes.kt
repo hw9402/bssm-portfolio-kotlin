8306ac7ccf8234a189c96688ce67554689ac2a53
마현우 <mhw35689402@gmail.com>
2023-12-26 14:56:11 +0900

    좋아요

---
+++ src/main/kotlin/bssmwintercamp/bssmportfolio/domain/entity/Likes.kt
@@ -0,0 +1,32 @@
+package bssmwintercamp.bssmportfolio.domain.entity
+
+import jakarta.persistence.*
+
+@Entity
+class Likes(
+    portfolio: Portfolio,
+    member: Member,
+): BaseDateTime() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "portfolio_id",
+        nullable = false,
+        foreignKey = ForeignKey(name = "FK_LIKES_PORTFOLIO_ID")
+    )
+    var portfolio: Portfolio = portfolio
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "member_id",
+        nullable = false,
+        foreignKey = ForeignKey(name = "FK_LIKES_MEMBER_ID")
+    )
+    var member: Member = member
+        protected set
+}
