d4ef96a4ef7c06db4604070f28b0fa3db34f8cc8
마현우 <mhw35689402@gmail.com>
2023-12-26 09:57:25 +0900

    sample test

---
+++ src/test/kotlin/bssmwintercamp/bssmportfolio/SampleTest.kt
@@ -0,0 +1,12 @@
+package bssmwintercamp.bssmportfolio
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class SampleTest: FunSpec({
+    test("sample test") {
+        println("Hello, world!")
+        1 shouldBe 1
+    }
+}) {
+}
